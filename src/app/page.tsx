e02750216f5faf84e4bfad6c6147205a543f8668
ecthon <ecthon@gmailcom>
2025-10-18 20:55:33 -0400

    feat: add Dialog component and integrate into CardBill for enhanced user interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <h1 className="font-bold text-lg">Dezembro</h1>
           <h1 className="font-normal text-zinc-400">/2025</h1>
         </div>
-        <p className="text-[12px] px-2">3/5</p>
+        <p className="text-[12px] px-2 text-zinc-400">3/5</p>
       </div>
       <div className="flex flex-col w-full space-y-0.5">
         <CardBill />
--- src/components/card-bill.tsx
+++ src/components/card-bill.tsx
@@ -2,6 +2,7 @@
 import { HugeiconsIcon } from '@hugeicons/react'
 import { AddInvoiceIcon, ArrowRight01Icon } from '@hugeicons/core-free-icons'
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
 
 export function CardBill() {
     return (
@@ -11,9 +12,23 @@ export function CardBill() {
                 <p className="text-sm max-sm:text-[13px] text-zinc-700">Cartão NuBank</p>
             </div>
             <div className='flex w-auto gap-2 items-center justify-between'>
-                <Button variant="ghost" size="icon-sm" aria-label="Anexar comprovante" className='max-sm:text-[12px] bg-zinc-50'>
-                    <HugeiconsIcon icon={AddInvoiceIcon} size={16} color="currentColor" strokeWidth={1.5} />
-                </Button>
+
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="ghost" size="icon-sm" aria-label="Anexar comprovante" className='max-sm:text-[12px] bg-zinc-50'>
+                            <HugeiconsIcon icon={AddInvoiceIcon} size={16} color="currentColor" strokeWidth={1.5} />
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Are you absolutely sure?</DialogTitle>
+                            <DialogDescription>
+                                This action cannot be undone. This will permanently delete your account
+                                and remove your data from our servers.
+                            </DialogDescription>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
                 <Button variant="ghost" size="icon-sm" aria-label="Anexar comprovante" className='max-sm:text-[12px] bg-zinc-50'>
                     <HugeiconsIcon icon={ArrowRight01Icon} size={16} color="currentColor" strokeWidth={1.5} />
                 </Button>
